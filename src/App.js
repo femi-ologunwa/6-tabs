b45f031d3f637d35d42f33069c156353c04156c0
Femi <ologunwafemi@gmail.com>
2021-05-06 14:34:12 +0100

    destructured & displayed items fetched from api

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const url = 'https://course-api.com/react-tabs-project';
 function App() {
    const [loading, setLoading] = useState(true);
    const [jobs, setJobs] = useState([]);
+   const [value, setValue] = useState(0);
 
    const fetchJobs = async () => {
       const response = await fetch(url);
@@ -26,7 +27,35 @@ function App() {
       );
    }
 
-   return <h2>jobs</h2>;
+   //destructuring and displaying details of a job object in jobs array
+   const { company, dates, duties, title } = jobs[value];
+
+   return (
+      <section className='section'>
+         <div className='title'>
+            <h2>experience</h2>
+            <div className='underline'></div>
+         </div>
+         <div className='jobs-center'>
+            {/* btn container */}
+
+            {/* job info */}
+            <article className='job-info'>
+               <h3>{title}</h3>
+               <h4>{company}</h4>
+               <p className='job-date'>{dates}</p>
+               {duties.map((duty, index) => {
+                  return (
+                     <div key={index} className='job-desc'>
+                        <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                        <p>{duty}</p>
+                     </div>
+                  );
+               })}
+            </article>
+         </div>
+      </section>
+   );
 }
 
 export default App;
