52b06f25ea12cb6fff67f20bde2091c46bd259f6
Femi <ologunwafemi@gmail.com>
2021-05-06 16:32:57 +0100

    Updates

---
+++ precache-manifest.a115961d7127096b9cfd83efa36153c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e71b9f5e0cb021d7be8dee01f30dc55",
+    "url": "/6-tabs/index.html"
+  },
+  {
+    "revision": "20884b6f55918a28f5f8",
+    "url": "/6-tabs/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/6-tabs/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/6-tabs/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20884b6f55918a28f5f8",
+    "url": "/6-tabs/static/js/main.cccd69bc.chunk.js"
+  },
+  {
+    "revision": "62f30d585ead5b20bbc2",
+    "url": "/6-tabs/static/js/runtime-main.c23dfbc3.js"
+  }
+]);
